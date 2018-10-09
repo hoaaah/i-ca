37bc17ccf06c650428be71d3314ca425503c17ef
Heru Arief Wijaya <hoaaah.arief@gmail.com>
2018-10-09 23:27:39 +0700

    UM

---
+++ _protected/modules/spd/views/um/connectionerror.php
@@ -0,0 +1,11 @@
+  <h2 class="headline text-yellow"> 404</h2>
+
+  <div class="error-content">
+    <h3><i class="fa fa-warning text-yellow"></i> Oops! Page not found.</h3>
+
+    <p>
+      Aplikasi saat ini tidak dapat terhubung kepada server aplikasi SPPD. Silahkan tunggu beberapa saat lagi, atau hubungi administrator.
+      Pesan Error: <?= $message ?>
+    </p>
+  </div>
+  <!-- /.error-content -->
